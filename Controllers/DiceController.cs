8d167a56b3185be6b64103baf6395f60784310a3
Codeacula <codeacula@codeacula.com>
2024-01-28 12:28:07 -0600

    Gonna convert to a console app

--- Controllers/DiceController.cs
+++ Controllers/DiceController.cs
@@ -7,6 +7,53 @@ namespace ShadowrunDiceRollerBot.Controllers
     [Route("dice")]
     public class DiceController : ControllerBase
     {
+        static readonly Random Rnd = new();
+
+        static List<int> GetRolls(int amount)
+        {
+            List<int> results = [];
+
+            for (int i = 0; i < amount; i++)
+            {
+                results.Add(Rnd.Next(1, 7));
+            }
+
+            return results;
+        }
+
+        static DiceRollResult RollDice(DiceRollParameters parameters)
+        {
+            var result = GetRolls(parameters.Amount);
+
+            if (parameters.Exploding) {
+                var tempResult = result;
+
+                while (tempResult.Any(x => x == parameters.ExplodeTheshold)) {
+                    var subresult = GetRolls(tempResult.Count(x => x == parameters.ExplodeTheshold));
+                    result.AddRange(subresult);
+                    tempResult = subresult;
+                }
+            }
+
+            var diceResult = new DiceRollResult
+            {
+                Hits = result.Count(x => x >= parameters.HitThreshold),
+                Results = result
+            };
+
+            if (parameters.Threshold.HasValue) {
+                diceResult.Success = diceResult.Hits >= parameters.Threshold.Value;
+            }
+
+            int ones = result.Count(x => x == 1);
+            int halfDice = (int)Math.Floor((decimal)diceResult.Results.Count / 2);
+
+            diceResult.Glitch = result.Count(x => x == 1) > diceResult.Hits;
+            diceResult.CriticalGlitch = ones >= halfDice;
+
+            return diceResult;
+        }
+
         [HttpGet]
         [Route("")]
         public ActionResult<string> Index()
@@ -18,7 +65,7 @@ namespace ShadowrunDiceRollerBot.Controllers
         [Route("roll")]
         public ActionResult<DiceRollResult> Roll(DiceRollParameters parameters)
         {
-            var result = new DiceRollResult();
+            var result = RollDice(parameters);
             return Ok(result);
         }
     }
--- Models/DiceRollParameters.cs
+++ Models/DiceRollParameters.cs
@@ -8,9 +8,19 @@ namespace ShadowrunDiceRollerBot.Models
         public int Amount { get; set; }
 
         /// <summary>
+        /// What should count as the dice threshold for exploding dice?
+        /// </summary>
+        public int ExplodeTheshold { get; set; } = 6;
+
+        /// <summary>
         /// Should 6's explode?
         /// </summary>
         public bool Exploding { get; set; } = false;
+
+        /// <summary>
+        /// Determines what counts as a hit. Sometimes edge may be used to lower the threshold to 4
+        /// </summary>
+        public int HitThreshold { get; set; } = 5;
 
         /// <summary>
         /// What Threshold are they trying to reach? Optional.
